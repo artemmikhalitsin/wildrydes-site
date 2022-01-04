b8eaa26a54d314a3700350276f7234145fe4bb4b
Artem Mikhalitsin <artemmikhalitsin@gmail.com>
2022-01-03 23:03:57 -0500

    Update cognito auth

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_U03TkGzV4', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4q39h4qeg64euq0rffgd41170l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6jmao28tcqs2jippl4rk13lugg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
